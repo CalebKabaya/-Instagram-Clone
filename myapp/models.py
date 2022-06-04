48e8f819ee8563f92b875ca773360ba23db4da8b
Moringa School <calebmutuku4@gmail.com>
2022-06-04 14:06:43 +0300

    ft:added the sighup form

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import DateField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -29,7 +30,15 @@ class Profile(models.Model):
     @classmethod
     def search_profile(cls,search_term) :
         profiles=cls.objects.filter(user__username__icontains=search_term) 
-        return profiles      
+        return profiles 
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()         
 
 class Post(models.Model):
     image=models.ImageField(upload_to='posts/')
