87139e53b967ddc59e26edd244fd5be2b6b129b1
Moringa School <calebmutuku4@gmail.com>
2022-06-04 16:50:41 +0300

    configured the login urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,10 @@ from django.urls import re_path
 from . import views
 
 urlpatterns=[
-   re_path('^$',views.welcome,name='welcome')
+   re_path('^$',views.welcome,name='welcome'),
+   re_path('register/', views.register, name='register'),
+   re_path('login/', views.login, name='login'),
+   re_path('logout/', views.logoutUser, name="logout"),
+
+
 ]
