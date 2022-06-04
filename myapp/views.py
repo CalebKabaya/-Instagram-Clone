1d425e04cea52eb3ce79466149cde9395c670ecd
Moringa School <calebmutuku4@gmail.com>
2022-06-04 18:45:40 +0300

    fixed a bug on login function

--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,14 @@ from django.contrib import messages
 from .forms import SignUpForm,UserCreationForm
 from .models import Profile, Post, User, Subscribers, Follow, Comment, Like
 from .emails import send_welcome_email
-from django.contrib.auth import login, authenticate
 from django.contrib.auth import authenticate, login, logout
+from .models import *
+
+
+from django.contrib import messages
+
+
+# Create your views here.
 
 
 # Create your views here.
@@ -36,7 +42,7 @@ def welcome(request):
 #             raw_password = form.cleaned_data.get('password1')
 #             user = authenticate(username=username, password=raw_password)
 #             login(request, user)
-#             return redirect('index')
+#             return redirect('login')
 #     else:
 #         form = SignUpForm()
 #     return render(request, 'users/register.html', {'form':form}) 
@@ -45,44 +51,82 @@ def welcome(request):
 
 #         return render(request, 'users/login.html',) 
 
-def register(request):
-	if request.user.is_authenticated:
-		return redirect('welcome')
-	else:
-		form = SignUpForm()
-		if request.method == 'POST':
-			form = SignUpForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
+# def register(request):
+# 	if request.user.is_authenticated:
+# 		return redirect('welcome')
+# 	else:
+# 		form = SignUpForm()
+# 		if request.method == 'POST':
+# 			form = SignUpForm(request.POST)
+# 			if form.is_valid():
+# 				form.save()
+# 				user = form.cleaned_data.get('username')
+# 				messages.success(request, 'Account was created for ' + user)
 
-				return redirect('login')
+# 				return redirect('login')
 			
 
-		context = {'form':form}
-		return render(request, 'users/register.html', context)
+# 		context = {'form':form}
+# 		return render(request, 'users/register.html', context)
 
 
-def  login(request):
-	# if request.user.is_authenticated:
-	# 	return redirect('welcome')
-	# else:
-	# 	if request.method == 'POST':
-	# 		username = request.POST.get('username')
-	# 		password =request.POST.get('password')
 
-	# 		user = authenticate(request, username=username, password=password)
+# def loginPage(request):
+# 	if request.user.is_authenticated:
+# 		return redirect('welcome')
+# 	else:
+# 		if request.method == 'POST':
+# 			username = request.POST.get('username')
+# 			password =request.POST.get('password')
 
-	# 		if user is not None:
-	# 			login(request, user)
-	# 			return redirect('welcome')
-	# 		else:
-	# 			messages.info(request, 'Username OR password is incorrect')
+# 			user = authenticate(request, username=username, password=password)
 
-		context = {}
-		return render(request, 'users/login.html', context)
+# 			if  user is not None:
+# 				login(request, user)
+# 				return redirect('welcome')
+# 			else:
+# 				messages.info(request, 'Username OR password is incorrect')
+
+# 		context = {}
+# 		return render(request, 'accounts/login.html', context)
 
 def logoutUser(request):
 	logout(request)
 	return redirect('login')
+
+
+
+def login_page(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('welcome')
+		else:
+			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
+			return redirect('login')	
+
+
+	else:
+		return render(request, 'users/login.html', {})
+
+
+def register(request):
+	if request.method == "POST":
+		form =SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Registration Successful!"))
+			return redirect('welcome')
+	else:
+		form = SignUpForm()
+
+	return render(request, 'users/register.html', {
+		'form':form,
+		})
