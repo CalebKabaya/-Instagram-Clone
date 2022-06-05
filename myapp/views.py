73fbcd9313048cef4c9fa74d55a37b3f0f659308
Moringa School <calebmutuku4@gmail.com>
2022-06-05 08:43:08 +0300

    added the function to post image

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import  redirect, render
-from django.http import HttpResponse, Http404
+from django.shortcuts import  get_object_or_404, redirect, render
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.contrib import messages
-from .forms import SignUpForm,UserCreationForm
+from .forms import SignUpForm,UpdateUserForm, UpdateUserProfileForm,PostForm
 from .models import Profile, Post, User, Subscribers, Follow, Comment, Like
 from .emails import send_welcome_email
 from django.contrib.auth import authenticate, login, logout
 from .models import *
-
+from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 
@@ -130,3 +130,81 @@ def register(request):
 	return render(request, 'users/register.html', {
 		'form':form,
 		})
+@login_required(login_url='login')
+def profile(request,username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    contex = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+        'images': images,
+
+    }
+    return render(request, 'accounts_pages/profile.html', contex)
+
+@login_required(login_url='login')
+def user_profile(request,username):
+    profile_user=get_object_or_404(User,username=username)
+    if request.user == profile_user:
+        return redirect('profile',username=request.user.username)
+    user_posts=profile_user.profile.posts.all()
+
+    followers=Follow.objects.filter(followed=profile_user.profile)  
+    following=None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            following=True  
+        else:
+            following=False
+    context = {
+        'profile_user':profile_user,
+        'user_posts':user_posts,
+        'followers':followers,
+        'following':following
+        }
+    return render(request, 'accounts_pages/user_profile.html', context)
+            
+@login_required(login_url='login')
+# def index(request):
+#     images = Post.objects.all()
+#     comments = Comment.objects.all()
+#     users = User.objects.exclude(id=request.user.id)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             image = form.save(commit = False)
+#             image.user = request.user.profile
+#             image.save()
+#             messages.success(request, f'Successfully uploaded your pic!')
+#             return redirect('index')
+#     else:
+#         form = PostForm()
+#     return render(request, 'index.html', {"images":images[::-1], "form": form, "users": users, "comments": comments })
+def post(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+    params = {
+        'images': images,
+        'form': form,
+        'users': users,
+
+    }
+    return render(request, 'index.html', params)
