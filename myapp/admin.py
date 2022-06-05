3af479255ecb57899f533491b6f6212753948193
Moringa School <calebmutuku4@gmail.com>
2022-06-05 08:44:18 +0300

    fixed a bug on post image

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile, Post, Like, Subscribers, Comment, Follow
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Subscribers)
+admin.site.register(Comment)
+admin.site.register(Follow)
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,12 @@ from django.urls import re_path
 from . import views
 
 urlpatterns=[
-   re_path('^$',views.welcome,name='welcome'),
+   re_path('^$',views.post,name='post'),
    re_path(r'^register/', views.register, name='register'),
    re_path(r'^login/', views.login_page, name='login'),
    re_path(r'^logout/', views.logoutUser, name="logout"),
+   re_path('profile/<username>/', views.profile, name='profile'),
+   re_path(r'^user_profile/<username>/', views.user_profile, name='user_profile'),
 
 
 
