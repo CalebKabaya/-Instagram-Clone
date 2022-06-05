db591baedd64cccfc267a51a6d0423216ce8d310
Moringa School <calebmutuku4@gmail.com>
2022-06-05 21:54:01 +0300

    fixed a bug on meadia settings

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,8 @@
 # from django.conf.urls import url
 from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
@@ -8,12 +11,26 @@ urlpatterns=[
    re_path(r'^register/', views.register, name='register'),
    re_path(r'^login/', views.login_page, name='login'),
    re_path(r'^logout/', views.logoutUser, name="logout"),
-   re_path('profile/<username>/', views.profile, name='profile'),
+   # re_path('profile/<username>/', views.profile, name='profile'),
+   re_path(r'^profile/<username>/', views.profile, name='profile'),
+
    re_path('user_profile/<username>/', views.user_profile, name='user_profile'),
    re_path(r'^image/(\d+)',views.image,name ='image'),
+   re_path(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+   re_path(r'^search/', views.search_results, name='search_results'),
+
+   # re_path('post/<id>', views.comment, name='comment'),
+    
+
+
+
 
 
 
 
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
