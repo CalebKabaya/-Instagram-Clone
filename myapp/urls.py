0f5aa6f78fe0af355ebdd21ed57b9072b4d68ff9
Moringa School <calebmutuku4@gmail.com>
2022-06-06 07:12:38 +0300

    added the license and the runtime.txt file

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,12 +12,14 @@ urlpatterns=[
    re_path(r'^login/', views.login_page, name='login'),
    re_path(r'^logout/', views.logoutUser, name="logout"),
    # re_path('profile/<username>/', views.profile, name='profile'),
-   re_path(r'^profile/<username>/', views.profile, name='profile'),
+   # re_path(r'^profile/<username>/', views.profile, name='profile'),
 
-   re_path('user_profile/<username>/', views.user_profile, name='user_profile'),
+   # re_path('user_profile/<username>/', views.user_profile, name='user_profile'),
    re_path(r'^image/(\d+)',views.image,name ='image'),
    re_path(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
    re_path(r'^search/', views.search_results, name='search_results'),
+   re_path('profile/<username>/', views.profile, name='profile'),
+   re_path('user_profile/<username>/', views.user_profile, name='user_profile'),
 
    # re_path('post/<id>', views.comment, name='comment'),
     
